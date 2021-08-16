c4e23a01fa4b42d9dabd7f707588c223b43cfb9e
karla-vasco <80479988+karla-vasco@users.noreply.github.com>
2021-08-16 15:27:57 -0400

    Códigos sobre estadística básica con R

---
+++ scripts/Estadistica_R_Glosario.R
@@ -0,0 +1,35 @@
+
+  ## * ¡Lo que aprendiste hoy! *
+  
+  ::: l-cuerpo-inicio
+| Opción | Descripción |
+  | ------------- | ----------------------------------- ----------------- |
+  | | ** Instalación y carga de paquetes ** |
+  | `install.packages` | Descargar e instalar paquetes desde repositorios similares a CRAN o desde archivos locales |
+  | `library` | Library y requieren cargar y adjuntar paquetes complementarios |
+  | | ** Usando datos existentes O cargando los suyos ** |
+  | `read_csv` | Leer un archivo de datos CSV (separados por comas) |
+  | `read_excel` | Leer archivos xls y xlsx |
+  | | ** Estructura de visualización de sus datos ** |
+  | `str` | Muestra la estructura interna de un objeto R; por ejemplo, si se almacena como una variable factorial (categórica), numérica u otro tipo de datos |
+  | `glimpse` | Echar un vistazo a tus datos |
+  | `resumen` | Ver valores de resumen simples |
+  | `View` | Invocar un visor de datos |
+  | `describe` | Ver estadísticas de un vistazo, requiere paquete R 'psych' |
+  | `ggplot + geom_histogram` | Trazar un histograma (`tidyverse` /` ggplot2`) |
+  | | ** Transformando sus datos ** |
+  | `log` | Toma el logaritmo de valores; usa ln (logaritmo natural, base e) por defecto |
+  | `log10` | Toma el logaritmo de base 10 |
+  | `scale` | Convertir datos a puntajes z |
+  | `rescale` | Cambie la escala de sus datos especificando una media y una DE (`psych`) |
+  | | ** Comprobación de la normalidad de las distribuciones ** |
+  | `shapiro.test` | Volver prueba de Shapiro-Wilk |
+  | `ks.test` | Volver prueba Kolmogorov-Smirnov |
+  | `median` | Devuelve el valor mediano |
+  | `mean` | Devuelve el valor medio |
+  | `sd` | Devuelve la desviación estándar |
+  | `var` | Devuelve la variación |
+  | | ** (Introducción a) Pruebas de asociación ** |
+  | `cor` | Devolver correlaciones simples |
+  | `corr.test` | Devuelve las correlaciones con más detalle, requiere el paquete R 'psych' |
+  :::
